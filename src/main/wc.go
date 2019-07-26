f86fcc9afed90aadfdfaf51bde405edd00fc012e
Krystian Wojcicki <kwojcicki@sympatico.ca>
2019-07-26 16:48:46 -0700

    Part 2 using mapreduce to code wc
    Signed-off-by: Krystian Wojcicki <kwojcicki@sympatico.ca>

--- src/main/wc.go
+++ src/main/wc.go
@@ -4,6 +4,9 @@ import (
 	"fmt"
 	"mapreduce"
 	"os"
+	"strconv"
+	"strings"
+	"unicode"
 )
 
 //
@@ -15,6 +18,16 @@ import (
 //
 func mapF(filename string, contents string) []mapreduce.KeyValue {
 	// Your code here (Part II).
+	kvs := make([]mapreduce.KeyValue, 0)
+	f := func(c rune) bool {
+		return !unicode.IsLetter(c) && !unicode.IsNumber(c)
+	}
+
+	for _, s := range strings.FieldsFunc(contents, f) {
+		kvs = append(kvs, mapreduce.KeyValue{Key: s, Value: s})
+	}
+
+	return kvs
 }
 
 //
@@ -24,6 +37,8 @@ func mapF(filename string, contents string) []mapreduce.KeyValue {
 //
 func reduceF(key string, values []string) string {
 	// Your code here (Part II).
+
+	return strconv.Itoa(len(values))
 }
 
 // Can be run in 3 ways:
